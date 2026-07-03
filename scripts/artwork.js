502c923c1ac91d23565761c1d1592222b9877df5
Becky Heaton <becky.a.heaton@outlook.com>
2026-07-03 20:14:29 +1000

    artwork pages

--- scripts/artwork.js
+++ scripts/artwork.js
@@ -5,33 +5,20 @@ fetch('artwork_indexing/artworks.json')
     .then(res => res.json())
     .then(artworks => {
         const art = artworks.find(a => a.id === id);
-        if (!art) { document.body.innerHTML = '<p>Artwork not found.</p>'; return; }
+        if (!art) { document.getElementById('artwork-page').innerHTML = '<p>Artwork not found.</p>'; return; }
 
-        const container = document.getElementById('artwork-page');
-        let mainContent = '';
+        const isVideo = art.type.includes('video') && art.videoLink;
+        const videoId = isVideo ? art.videoLink.split('youtu.be/')[1].split('?')[0] : null;
 
-        if (art.type.includes('video') && art.videoLink) {
-            const videoId = art.videoLink.split('youtu.be/')[1];
-            mainContent = `<iframe width="560" height="315"
-                src="https://www.youtube.com/embed/${videoId}"
-                frameborder="0" allowfullscreen></iframe>`;
+        const media = isVideo
+            ? `<iframe src="https://www.youtube.com/embed/${videoId}" allowfullscreen></iframe>`
+            : `<img src="${art.thumbnail}" alt="${art.name}">`;
 
-        } else if (art.type.includes('text') && art.documentLink) {
-            fetch(art.documentLink)
-                .then(r => r.text())
-                .then(text => {
-                    document.getElementById('main').innerText = text;
-                });
-            mainContent = `<pre id="main"></pre>`;
-
-        } else {
-            mainContent = `<img src="${art.thumbnail}" alt="${art.name}">`;
-        }
-
-        container.innerHTML = `
-            <p>${art.name}</p>
-            <p>${art.date ?? ''}</p>
-            ${mainContent}
-            <p>${art.description ?? ''}</p>
+        document.getElementById('artwork-page').innerHTML = `
+            <h1 class="artwork-title">${art.name}</h1>
+            <div class="artwork-content">
+                <div class="artwork-media">${media}</div>
+                <p class="artwork-description">${art.description ?? ''}</p>
+            </div>
         `;
     });
